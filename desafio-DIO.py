60b01ed2f9db306a0ea5752914afbb8f9fc2854a
mrblackp <marcos.vnascimento0@gmail.com>
2025-04-13 11:57:02 -0300

    desafio-dio

---
+++ desafio-DIO.py
@@ -0,0 +1,5 @@
+print("Estou aprendendo a usar o git e o github\nE este é meu primeiro desafio no github com a DIO")
+
+print("Irei começar com o básico e imprimir a tão famosa frase Hello World!\nE em seguida uma operação matemática simples\n")
+print("Hello World!")
+print("A soma de 50 + 50 é igual a: ", 50 + 50)
